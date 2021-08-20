096d11c5eec543a9bbeafd020f1babe17bf4dd2a
Sophia Kim <mail@fifikim.com>
2021-08-20 09:59:14 -0400

    add gitignore file

--- src/polybius.js
+++ src/polybius.js
@@ -6,14 +6,14 @@ const polybiusModule = (function () {
     m: '23', n: '33', o: '43', p: '53', q: '14', r: '24', 
     s: '34', t: '44', u: '54', v: '15', w: '25', x: '35', 
     y: '45', z: '55'
-  }
+  };
   const decodeKey = {
     11: 'a', 21: 'b', 31: 'c', 41: 'd', 51: 'e', 12: 'f', 
     22: 'g', 32: 'h', 42: '(i/j)', 52: 'k', 13: 'l', 
     23: 'm', 33: 'n', 43: 'o', 53: 'p', 14: 'q', 24: 'r', 
     34: 's', 44: 't', 54: 'u', 15: 'v', 25: 'w', 35: 'x', 
     45: 'y', 55: 'z'
-  }
+  };
   
   function polybius(input, encode = true) {
     input = input.toLowerCase(); // treats caps as lowercase
